ac7512d0209c4b22a1bd63f0e083c3b14ea97fa1
Igor Leme <igor.leme.m@outlook.com>
2022-02-13 10:20:17 -0300

    Creating the project.

---
+++ src/main/java/br/com/iv/qikserve/QikserveApplication.java
@@ -0,0 +1,13 @@
+package br.com.iv.qikserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QikserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QikserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/iv/qikserve/QikserveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.iv.qikserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QikserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
