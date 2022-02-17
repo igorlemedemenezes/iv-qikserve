9037679566f389b97cd4dda74eac6ce77634fb96
Igor Leme <igor.leme.m@outlook.com>
2022-02-17 03:21:36 -0300

    ControllerAdvice to costumize exception

---
+++ src/main/java/br/com/iv/qikserve/controller/exception/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package br.com.iv.qikserve.controller.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.iv.qikserve.exceptions.ObjectNotFoundException;
+
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Object not found.", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+}
