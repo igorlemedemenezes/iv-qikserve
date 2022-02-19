5e95ac320882587789ea73521f6ab7abb863a541
Igor Leme <igor.leme.m@outlook.com>
2022-02-19 01:23:08 -0300

    Allows cors configuration to communication with front-end

--- src/main/java/br/com/iv/qikserve/security/config/SecurityConfig.java
+++ src/main/java/br/com/iv/qikserve/security/config/SecurityConfig.java
@@ -64,12 +64,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+//		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000/"));
 		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+	    configuration.setAllowedHeaders(Arrays.asList("X-Requested-With","Origin","Content-Type","Accept","Authorization"));
+	    // This allow us to expose the headers
+	    configuration.setExposedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Authorization, x-xsrf-token, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, " +
+	            "Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"));
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
-
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
